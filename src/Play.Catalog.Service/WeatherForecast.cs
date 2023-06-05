0fe02bc30b1880832d88875affb097d0c961e58c
Natyari Vargas <naryari@gmail.com>
2023-06-05 12:05:39 -0600

    Pasando a la versión 5 de .net

--- src/Play.Catalog.Service/WeatherForecast.cs
+++ src/Play.Catalog.Service/WeatherForecast.cs
@@ -1,12 +1,15 @@
-namespace Play.Catalog.Service;
+using System;
 
-public class WeatherForecast
+namespace Play.Catalog.Service
 {
-    public DateOnly Date { get; set; }
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
 
-    public int TemperatureC { get; set; }
+        public int TemperatureC { get; set; }
 
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-    public string? Summary { get; set; }
+        public string Summary { get; set; }
+    }
 }
